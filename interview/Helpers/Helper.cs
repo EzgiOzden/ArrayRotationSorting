8607a7a82e599d5de81ba58cbffd44692ec46c3e
Ezgi <Ezgi@DESKTOP-1I85VPG>
2020-01-29 22:01:17 +0300

    Add project files.

---
+++ interview/Helpers/Helper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+
+namespace interview.Helpers
+{
+    public static class Helper
+    {
+        public static string MessageInputTestCounter { get; set; } = "Input Test Case Counter: ";
+        public static string MessageInputArrayLength { get; set; } = "Input Array Length: ";
+
+        public static int InputTestCounter { get; set; } = 0;
+        public static int InputArrayLength { get; set; } = 0;
+        public static int[] InputArray { get; set; } = new int[0];
+
+
+        public static void InputLoading()
+        {
+            bool IsLoadInputValue = true;
+            //TODO: connection two do while operation
+            do
+            {
+                InputArrayLength = GetInputData(MessageInputArrayLength);
+                if (InputArrayLength == 0)
+                    IsLoadInputValue = true;
+                else
+                    IsLoadInputValue = false;
+            } while (IsLoadInputValue);
+
+            do
+            {
+                var IsArrayLoading = GetArrayValues();
+                if (IsArrayLoading)
+                {
+                    IsLoadInputValue = true;
+                }
+                else
+                {
+                    IsLoadInputValue = false;
+                }
+            } while (IsLoadInputValue);
+
+
+        }
+
+        public static int GetInputData(string message, int callBackInput = 0)
+        {
+            Console.Write(message);
+
+            if (callBackInput != 0)
+            {
+                Console.Write(callBackInput.ToString());
+                return callBackInput;
+            }
+
+            var CasesInput = int.TryParse(Console.ReadLine(), out callBackInput);
+            if (!CasesInput)
+                return 0;
+
+            return callBackInput;
+        }
+
+        private static bool GetArrayValues()
+        {
+            int[] _inputArray = new int[InputArrayLength];
+
+            Console.Write("Array Giriniz: ");
+            string readLine = Console.ReadLine();
+            string[] stringArray = readLine.Split(' ');
+
+            if (stringArray.Length != InputArrayLength)
+            {
+                Console.WriteLine("Array boyutundan farklı girdiniz tekrar deneyin.");
+                return true;
+            }
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                var controlElement = int.TryParse(stringArray[i], out _inputArray[i]);
+                if (!controlElement)
+                {
+                    return true;
+                }
+            }
+
+            var checkSequence = ControlArrayElement(_inputArray);
+            if (!checkSequence)
+            {
+                Console.WriteLine("Ardışık sayılar giriniz.");
+                return true;
+            }
+
+            InputArray = _inputArray;
+
+            return false;
+        }
+
+        private static bool ControlArrayElement(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (!inputArray.Contains(i + 1))
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
---
+++ interview/Program.cs
@@ -0,0 +1,207 @@
+﻿using interview.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace interview
+{
+    class Program
+    {
+        public static List<int> SortedElements { get; set; } = new List<int>();
+        public static bool IsSorted { get; set; } = true;
+        public static List<string> Result { get; set; } = new List<string>();
+
+        static void Main(string[] args)
+        {
+            bool IsLoadInputValue = true;
+            do
+            {
+                Helper.InputTestCounter = Helper.GetInputData(Helper.MessageInputTestCounter);
+                if (Helper.InputTestCounter == 0)
+                    IsLoadInputValue = true;
+                else
+                    IsLoadInputValue = false;
+            } while (IsLoadInputValue);
+
+            for (int i = 0; i < Helper.InputTestCounter; i++)
+            {
+                string consoleResult = null;
+                do
+                {
+                    Helper.InputLoading();
+
+                    var control = SortControl();
+                    IsSorted = !control.IsSortedSuccess;
+                    consoleResult = control.IsResultTrue ? "YES" : "NO";
+
+                } while (IsSorted);
+                Result.Add(consoleResult);
+            }
+
+            foreach (var result in Result)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static ResponseModel SortControl()
+        {
+            ResponseModel response;
+            SortedElements.Clear();
+
+
+            if (Helper.InputArray.Count() < 3)
+            {
+                return response = new ResponseModel()
+                {
+                    IsResultTrue = false,
+                    IsSortedSuccess = true
+                };
+            }
+
+            for (int i = 0; i < Helper.InputArray.Length; i++)
+            {
+                if (i + 1 != Helper.InputArray[i])
+                {
+                    var con = true;
+                    do
+                    {
+                        if (i + 1 == Helper.InputArray[i])
+                        {
+                            SortedElements.Add(Helper.InputArray[i]);
+                            con = false;
+                            break;
+                        }
+
+                        var orderIndex = Array.IndexOf(Helper.InputArray, i + 1);
+                        var rotationElements = GetElementForRotation(orderIndex);
+
+                        if (rotationElements.Count() == 0 || rotationElements == null)
+                        {
+                            return response = new ResponseModel()
+                            {
+                                IsResultTrue = false,
+                                IsSortedSuccess = true
+                            };
+                        }
+
+                        Helper.InputArray = Rotation(Helper.InputArray, rotationElements[0], rotationElements[1], rotationElements[2]);
+
+                    } while (con);
+
+
+                }
+                else
+                {
+                    SortedElements.Add(Helper.InputArray[i]);
+                }
+            }
+
+            for (int i = 0; i < SortedElements.ToArray().Length; i++)
+            {
+                if (SortedElements[i] != i + 1)
+                {
+                    return response = new ResponseModel()
+                    {
+                        IsResultTrue = false,
+                        IsSortedSuccess = true
+                    };
+                }
+            }
+
+            if (SortedElements.Count() != Helper.InputArray.Length)
+            {
+                return response = new ResponseModel()
+                {
+                    IsResultTrue = false,
+                    IsSortedSuccess = true
+                };
+            }
+
+            return response = new ResponseModel()
+            {
+                IsResultTrue = true,
+                IsSortedSuccess = true
+            };
+        }
+
+        public static int[] GetElementForRotation(int orderIndex)
+        {
+            List<int> rotationElements = new List<int>();
+
+            try
+            {
+                if (!SortedElements.Contains(Helper.InputArray[orderIndex - 1]) && !SortedElements.Contains(Helper.InputArray[orderIndex - 2]))
+                {
+                    rotationElements.Add(Helper.InputArray[orderIndex - 2]);
+                    rotationElements.Add(Helper.InputArray[orderIndex - 1]);
+                    rotationElements.Add(Helper.InputArray[orderIndex]);
+
+                    return rotationElements.ToArray();
+                }
+
+                else if (!SortedElements.Contains(Helper.InputArray[orderIndex - 1]) && !SortedElements.Contains(Helper.InputArray[orderIndex + 1]))
+                {
+                    rotationElements.Add(Helper.InputArray[orderIndex - 1]);
+                    rotationElements.Add(Helper.InputArray[orderIndex]);
+                    rotationElements.Add(Helper.InputArray[orderIndex + 1]);
+
+                    return rotationElements.ToArray();
+                }
+
+                else if (!SortedElements.Contains(Helper.InputArray[orderIndex]) && SortedElements.Contains(Helper.InputArray[orderIndex - 2]) || SortedElements.Contains(Helper.InputArray[orderIndex - 1]))
+                {
+                    return rotationElements.ToArray();
+                }
+
+                return rotationElements.ToArray();
+            }
+            catch (Exception)
+            {
+                if (Helper.InputArray.Count() - SortedElements.Count() < 3)
+                {
+                    return rotationElements.ToArray(); 
+                }
+
+                if (orderIndex + 1 <= Helper.InputArray.Length)
+                {
+                    rotationElements.Add(Helper.InputArray[orderIndex - 1]);
+                    rotationElements.Add(Helper.InputArray[orderIndex]);
+                    rotationElements.Add(Helper.InputArray[orderIndex + 1]);
+
+                    return rotationElements.ToArray();
+                }
+                else
+                {
+                    return rotationElements.ToArray();
+                }
+            }
+        }
+
+        public static int[] Rotation(int[] ABCArray, int A, int B, int C)
+        {
+            int AValue = ABCArray.FirstOrDefault(p => p == A);
+            int AIndex = Array.IndexOf(ABCArray, A);
+
+            int BValue = ABCArray.FirstOrDefault(p => p == B);
+            int BIndex = Array.IndexOf(ABCArray, B);
+
+            int CValue = ABCArray.FirstOrDefault(p => p == C);
+            int CIndex = Array.IndexOf(ABCArray, C);
+
+            ABCArray[AIndex] = BValue;
+            ABCArray[BIndex] = CValue;
+            ABCArray[CIndex] = AValue;
+
+            return ABCArray;
+        }
+    }
+
+    public class ResponseModel
+    {
+        public bool IsSortedSuccess { get; set; }
+        public bool IsResultTrue { get; set; }
+    }
+}
